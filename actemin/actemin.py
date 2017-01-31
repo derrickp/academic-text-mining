c3078ad830906af5985ecc37fd9b225e336d8e98
Derrick Plotsky <derrickplotsky@gmail.com>
2017-01-30 20:46:03 -0800

    Work on pdflooper
     - Work on pdflooper.py
     - Output txt files added
     - remove __pycache__
     - Add to .gitignore
     - Remove virtualenv folders from settings.json and .gitignore

--- actemin/actemin.py
+++ actemin/actemin.py
@@ -1,3 +1,17 @@
-from pdflooper import get_pdf_filenames
+""" actemin package is used to read text from PDFs and then send that through spaCy
+"""
 
-get_pdf_filenames("test")
+from pdflooper import get_pdf_filenames, write_text_file
+
+def main():
+    """ main function for actemin
+    """
+    file_names = get_pdf_filenames(".\\data")
+
+    out_path = ".\\out_txt"
+    for file_name in file_names:
+        success = write_text_file(file_name, out_path)
+        print(file_name + " " + str(success))
+
+if __name__ == "__main__":
+    main()
